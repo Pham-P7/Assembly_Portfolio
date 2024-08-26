036138b226df1324578747532e962a01a89253c5
Phat Pham <101293853+Pham-P7@users.noreply.github.com>
2024-08-26 15:04:00 -0500

    Update xyz geom n cube.py

--- xyz geom n cube.py
+++ xyz geom n cube.py
@@ -1,6 +1,5 @@
 def cube(x,y,z,n):
-    cubelist = [(x+1) * (y+1) * (z+1)][3]
-    for i in range(0,len(cubelist)):
-        cubelist[i] = [(a,b,c) for a in range(0,x + 1) for b in range(0,y + 1) for c in range(0,z +1) if a+b+c != n]
+    cubelist = [][3]
+    cubelist[] = [(a,b,c) for a in range(0,x + 1) for b in range(0,y + 1) for c in range(0,z +1) if a+b+c != n]
     print(cubelist)
 cube(1,1,2,3)
