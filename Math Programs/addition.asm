3f68c9be1cb60d0915dbe864242dea31d97ecdfc
Phat Pham <parentpham07@gmail.com>
2024-10-03 14:47:15 -0500

    updated

--- Math Programs/addition.asm
+++
@@ -1,5 +0,0 @@
-use64
-macro add5 target* {
-  add target, 5 
-}
-add5 rax ;added 5 to register rax
---
+++ fASM Programs/Math Programs/addition.asm
@@ -0,0 +1,5 @@
+use64
+macro add5 target* {
+  add target, 5 
+}
+add5 rax ;added 5 to register rax
